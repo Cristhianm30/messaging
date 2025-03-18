05d219c19bf254bb2cbce4941242a24af4a23c35
Cristhian Andres Moreno Velasco <Cristhian.452@outlook.com>
2025-03-18 14:46:53 -0500

    respuesta para la HU15

--- src/main/java/com/pragma/powerup/application/dto/response/OtpResponseDto.java
+++ src/main/java/com/pragma/powerup/application/dto/response/OtpResponseDto.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class OtpResponseDto {
+    private String otp;
     private String phone;
     private String message;
 }
